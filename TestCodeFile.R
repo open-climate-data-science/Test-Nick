d218f52740b842ccd8054578905365fcb3d89eb8
ncgawron <nickgawron@gmail.com>
2021-12-07 01:32:15 -0500

    Update TestCodeFile.R
    I added some variables in the R code.

--- TestCodeFile.R
+++ TestCodeFile.R
@@ -1,6 +1,11 @@
 #Here is a test R file 
 
 vec<-c(1,2,3,4,5)
+vec2<- c(4,5,6,9,11)
 
-mult2<-2*vec
-mult2
+model1<-lm(vec2~vec)
+
+summary(model1)
+# I added some R code
+plot(vec,vec2)
+abline(model1)
