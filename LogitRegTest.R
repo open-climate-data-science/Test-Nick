96ad0a2c5d360c3fad9f15c37b7856006c6bb2ad
ncgawron <nickgawron@gmail.com>
2021-12-15 16:47:16 -0500

    Added New R file
    Simple idea for basic tutorial  and practice on git commands

---
+++ LogitRegTest.R
@@ -0,0 +1,42 @@
+# Another test R file 
+# logistic regression basic tutorial idea
+
+
+#subets data
+# consdiers all rows that are not the third level
+irissub<-subset(iris, Species != levels(iris$Species)[3])
+
+# table of frequecy 
+table(irissub$Species)
+
+# new set of levels 
+levels(irissub$Species)
+
+# drops the only level virginica
+irissub$Species<-droplevels(irissub)$Species
+
+
+
+# makes new coloumn for response varable  
+irissub$ISversi <- as.integer(irissub$Species=="versicolor")
+
+
+
+# makes logistic regression model 
+model1<-glm(ISversi~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,data=irissub,family="binomial")
+model1
+
+# summary of model 
+summary(model1)
+
+
+
+# plot that shows this
+plot(irissub$Sepal.Length,jitter(irissub$ISversi,.1),col="red",xlab="Sepal Length",
+     ylab="Species",main="Probability Plot of Versicolor Species")
+# linear regrssion (not so good here)
+lines(lowess(irissub$ISversi~irissub$Sepal.Length),col="green")
+
+# Logit regression curve
+lines(irissub$Sepal.Length[order(irissub$Sepal.Length)], fitted(model1)[order(irissub$Sepal.Length)],lty=2,col="blue")
+
