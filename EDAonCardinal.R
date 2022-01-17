7b09b5ca118507b21512415b25a938fe83c9910c
ncgawron <nickgawron@gmail.com>
2022-01-16 23:24:13 -0500

    Adding SLR
    Simple linear regression cardinals data.
    Note the plot is not working right now!

--- EDAonCardinal.R
+++ EDAonCardinal.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 #reads in the data 
 CardinalSD <- read_excel("CardinalSampleData.xlsx")
-View(CardinalSampleData)
+View(CardinalSD)
 
 #converts to a date for
 CardinalSD$Date<-as.Date(CardinalSD$Date)
@@ -16,3 +16,22 @@ ggplot(stack(CardinalSD[,-1]))+geom_boxplot(aes(y=values))+facet_wrap(~ind,scale
 
 #simple density plot of one variable
 ggplot((CardinalSD[,-1]))+geom_density(aes(x=AvgTemp))
+
+
+#### SAMPLE LOGISTIC REGRESSION!!!
+
+# new categorical variable
+
+CardinalSD$RainYN<- as.factor(as.numeric(CardinalSD$Precp>0))
+CardinalSD
+
+
+mod1.logit <- glm(RainYN ~ AvgHumid,data=CardinalSD, family=binomial)
+
+summary(mod1.logit)
+
+
+plot(CardinalSD$AvgHumid, jitter((as.numeric(CardinalSD$Precp>0)),.1))
+
+lines(CardinalSD$AvgHumid[order(CardinalSD$AvgHumid)],fitted(mod1.logit)[order(CardinalSD$AvgHumid)],lty=2,col=3)
+
